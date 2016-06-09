61dd322c02a93001b4a493a52caae70ecf98cc0f
Erika Siregar <erikaris1515@gmail.com>
2016-06-09 11:43:08 +0700

    Update start.py

--- start.py
+++ start.py
@@ -2,24 +2,25 @@ from app import application
 from gevent import wsgi
 from tornado import httpserver, ioloop
 from tornado.wsgi import WSGIAdapter
+import sys
 
+port = sys.argv[1] if len(sys.argv) > 0 else application.settings.get('port')
 
 def start_server():
     server = httpserver.HTTPServer(application)
     # server.listen(application.settings.get('port'),
     #                    address='0.0.0.0')
-    server.bind(application.settings.get('port'),
-                       address='0.0.0.0')
-    server.start(4) # Specify number of subprocesses
+    server.bind(port, address='0.0.0.0')
+    server.start() # Specify number of subprocesses
     
     print('Server is started at {}:{}'.format(
-        'localhost', application.settings.get('port')))
+        'localhost', port))
     
     ioloop.IOLoop.current().start()
 
 
 def start_gevent():
-    server = wsgi.WSGIServer(('0.0.0.0', application.settings.get('port')),
+    server = wsgi.WSGIServer(('0.0.0.0', port),
                              WSGIAdapter(application))
     server.serve_forever()
     ioloop.IOLoop.current().start()
