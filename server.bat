6b1fc00af0a7560a05a8a275a00a9f7c0a2598c3
erikaris <erikaris1515@gmail.com>
2016-11-30 17:17:56 +0700

    Remove “Press Enter key to exit..”

--- server.bat
+++ server.bat
@@ -1,2 +1,1 @@
 python.exe server.py %*
-pause
--- server.sh
+++ server.sh
@@ -4,5 +4,3 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 cd "$parent_path"
 
 python server.py $1 $2
-
-read -p "Press [Enter] key to exit..."
